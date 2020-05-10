bab4a2ab08c206a2bec62ed3e63a4cc31cb3f254
Luke Chen <luke.chen.2010@gmail.com>
2020-05-10 01:03:56 +0000

    Automatically backed up by Learn

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -60,12 +60,12 @@ describe 'Song' do
       Artist.class_variable_set("@@all",[])
 
       song = Song.new('Man in the Mirror')
-      song.artist_name("Michael Jackson")
+      song.artist_name = "Michael Jackson"
       expect(song.artist).to be_an(Artist)
       expect(song.artist.name).to eq("Michael Jackson")
 
       song_2 = Song.new('Thriller')
-      song_2.artist_name("Michael Jackson")
+      song_2.artist_name "Michael Jackson")
       expect(song_2.artist).to be_an(Artist)
       expect(song_2.artist.name).to eq("Michael Jackson")
 
