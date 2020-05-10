93800f55fa61205088d17e3bbcd63f9cbf464a91
Luke Chen <luke.chen.2010@gmail.com>
2020-05-10 00:27:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
     file_name_split = file_name.split(" - ")
     song = Song.new(file_name_split[1])
     artist = Artist.new(file_name_split[0])
-    song.artist_name = artist
+    song.artist = artist
     return song
   end
 end
